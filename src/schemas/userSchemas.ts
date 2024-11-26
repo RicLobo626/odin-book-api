61330d915a2b64d1a57f3538f81b44314beeae15
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-11-26 19:29:13 +0000

    Include posts in user queries and update Zod schemas/types

--- src/schemas/userSchemas.ts
+++ src/schemas/userSchemas.ts
@@ -1,27 +1,24 @@
+import { postSchema } from "@/schemas/postSchemas.js";
 import { z } from "zod";
 
 export const userSchema = z.object({
   id: z.number(),
-  firstName: z.string().min(1),
-  lastName: z.string().min(1),
+  fullName: z.string().min(1),
   email: z.string().email(),
   createdAt: z.date(),
-  password: z.string().min(60), // hashed password
+  posts: z.array(postSchema),
 });
 
-const password = z.string().min(5).max(50);
-
-export const newUserSchema = z.object({
-  firstName: z.string().min(1),
-  lastName: z.string().min(1),
-  email: z.string().email(),
-  password,
-});
+export const newUserSchema = userSchema
+  .pick({
+    email: true,
+  })
+  .extend({
+    firstName: z.string().min(1),
+    lastName: z.string().min(1),
+    password: z.string().min(5).max(50),
+  });
 
 export const loginSchema = newUserSchema.pick({ email: true, password: true });
 
-export const publicUserSchema = userSchema
-  .omit({ firstName: true, lastName: true, password: true, email: true })
-  .extend({
-    fullName: z.string().min(2),
-  });
+export const publicUserSchema = userSchema.omit({ email: true });
--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -2,10 +2,10 @@ import db from "@/db/index.js";
 import { NotFoundError } from "@/errors/index.js";
 import { NewUser, User } from "@/types/index.js";
 
-const findUsers = () => db.user.findMany();
+const findUsers = () => db.user.findMany({ include: { posts: true } });
 
 const findUserById = async (id: User["id"]) => {
-  const user = await db.user.findUnique({ where: { id } });
+  const user = await db.user.findUnique({ where: { id }, include: { posts: true } });
 
   if (!user) {
     throw new NotFoundError("User not found");
@@ -14,7 +14,7 @@ const findUserById = async (id: User["id"]) => {
   return user;
 };
 
-const createUser = (data: NewUser) => db.user.create({ data });
+const createUser = (data: NewUser) => db.user.create({ data, include: { posts: true } });
 
 export default {
   findUsers,
